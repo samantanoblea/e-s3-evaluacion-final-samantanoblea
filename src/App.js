1997030a2a781b96e3d1ce023a31ba4ef8dff64e
Samanta Noblea <samantanoblea@gmail.com>
2019-01-24 09:50:43 +0100

    add filter

--- src/App.js
+++ src/App.js
@@ -6,35 +6,58 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      query:'',
       results: []
     }
     this.getWizards()
 
   this.getWizards = this.getWizards.bind(this);
+  this.getQuery = this.getQuery.bind(this);
+  
+
+
   }
 
   getWizards(){
     fetchWizards()
       .then(data => {
+        const newData = data.map((item, index) =>{return {...item,id:index}})
+
         this.setState({
-          results: data
+          results: newData
         })
       })
   };
 
+  getQuery(event){
+    const userQuery = event.currentTarget.value;
+    this.setState({
+      query: userQuery
+    })
+  }
+
+  getFilter(){
+    const filterResults = this.state.results.filter(item =>{
+    
+    return (item.name.includes(this.state.query)?true :false)
+    });
+    return filterResults;
+  }
 
 
   render() {
+    const filterResults = this.getFilter();
     return (
       <div className="app">
         <header className="app__header">
           <h1 className="header__title">Harry Potter Characters</h1>
+          <input className="header__filter" onKeyUp={this.getQuery}></input>
         </header>
         <main className="app__main">
           <ul className="wizards__list">
-            {this.state.results.map(item =>{
+            {filterResults.map(item =>{
               return(
-                <li className="wizard__item">
+                <li className="wizard__item" key={item.id}>
                   <div className="">
                     <h2 className="wizard__name">{item.name}</h2>
                     <img className="wizard__image" src={item.image} alt={item.name}></img>
