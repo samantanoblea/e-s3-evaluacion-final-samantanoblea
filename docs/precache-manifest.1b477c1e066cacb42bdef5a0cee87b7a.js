47dea8643c8f5eb4c9dc2fbc42237c3f82119b1e
Samanta Noblea <samantanoblea@gmail.com>
2019-01-25 13:17:31 +0100

    gitHub pages

---
+++ docs/precache-manifest.1b477c1e066cacb42bdef5a0cee87b7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "226901ea896c673cd36bf724ed1f273d",
+    "url": "./static/media/ravenclaw.226901ea.jpeg"
+  },
+  {
+    "revision": "ade76d3c29e132c23cb068547f77e19a",
+    "url": "./static/media/hufflepuff.ade76d3c.jpeg"
+  },
+  {
+    "revision": "839cdc37102837b437a914718500ae7a",
+    "url": "./static/media/gryffindor.839cdc37.jpeg"
+  },
+  {
+    "revision": "c3af48a9463126cb2c18ef59d090dbc8",
+    "url": "./static/media/Hogwarts.c3af48a9.jpeg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "195ed5e36f98540f50c2",
+    "url": "./static/js/main.195ed5e3.chunk.js"
+  },
+  {
+    "revision": "5fba46790563a5c77bfc",
+    "url": "./static/js/1.5fba4679.chunk.js"
+  },
+  {
+    "revision": "195ed5e36f98540f50c2",
+    "url": "./static/css/main.a6917326.chunk.css"
+  },
+  {
+    "revision": "be9ad246d1a0e139952a206f4c241da3",
+    "url": "./index.html"
+  }
+];
